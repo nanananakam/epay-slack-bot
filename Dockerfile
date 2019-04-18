9806ee8c3514bd574f812f67a6c4b6c19b539101
Naoto Nakamura <naoto.nakamura@dena.com>
2019-04-18 19:24:42 +0900

    epay打刻を移植した

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,21 @@ FROM golang:1.12 as builder
 ENV CGO_ENABLED=0
 ENV GOOS=linux
 ENV GOARCH=amd64
+ENV TZ=Asia/Tokyo
 
 COPY . /
-RUN go build -o /main /main.go
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add \
+ && echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list \
+ && apt-get update && apt-get -y install google-chrome-stable unzip \
+ && CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` \
+ && wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/ \
+ && unzip ~/chromedriver_linux64.zip -d ~/ \
+ && rm ~/chromedriver_linux64.zip \
+ && chown root:root ~/chromedriver \
+ && chmod 755 ~/chromedriver \
+ && mv ~/chromedriver /usr/bin/chromedriver \
+ && apt-get -y remove unzip \
+ && apt clean && rm -rf /var/lib/apt/lists/* \
+ && go build -o /main /main.go
 CMD /main $PORT
 
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/sclevine/agouti"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 type SlackMsg struct {
@@ -83,7 +86,7 @@ func BindSlackData(_ http.ResponseWriter, r *http.Request) {
 
 	log.Println("config setting:", cfg.WebhookUrl)
 	if postData.UserName == cfg.TargetUser {
-		SlackPost(cfg, "はい")
+		SlackPost(cfg, epay())
 	} else {
 		SlackPost(cfg, "@"+postData.UserName+" お前は誰だ")
 	}
@@ -93,4 +96,124 @@ func main() {
 	log.Println("server startup......")
 	http.HandleFunc("/v1/slack/inbound", BindSlackData)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func epay() string {
+	var result string
+
+	driver := agouti.ChromeDriver(agouti.ChromeOptions("args", []string{
+		"--headless",
+		"--no-sandbox",
+		"--disable-gpu",
+		"--disable-extensions",
+		"--disable-print-preview",
+		"--ignore-certificate-error",
+		"--window-size=640,480",
+	}), agouti.Debug)
+
+	err := driver.Start()
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Navigate("https://prb01.payroll.co.jp/epayc/")
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Find("input[name=\"copCd\"]").Fill(os.Getenv("ePayWorkCopCd"))
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Find("input[name=\"empCd\"]").Fill(os.Getenv("ePayWorkEmpCd"))
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Find("input[name=\"password\"]").Fill(os.Getenv("ePayWorkPassword"))
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Find("button[type=\"submit\"]").Click()
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Navigate("https://prb01.payroll.co.jp/epayc/mainPersonal.do?op=doSso&fwdSyscd=work&concd=calendar")
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	now := time.Now()
+
+	fromValue, err := page.All(".work-control.work-calendar").At(now.Day() - 1).All("input[type=\"text\"]").At(0).Attribute("value")
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	mmss := fmt.Sprintf("%02d%02d", now.Hour(), now.Minute())
+	if fromValue == "" {
+		err = page.All(".work-control.work-calendar").At(now.Day() - 1).All("input[type=\"text\"]").At(0).Fill(mmss)
+		if err != nil {
+			driver.Stop()
+			log.Println(err)
+			return err.Error()
+		}
+		result = "出勤打刻 " + mmss
+	} else {
+		err = page.All(".work-control.work-calendar").At(now.Day() - 1).All("input[type=\"text\"]").At(1).Fill(mmss)
+		if err != nil {
+			driver.Stop()
+			log.Println(err)
+			return err.Error()
+		}
+		result = "退勤打刻 " + mmss
+	}
+
+	err = page.Find("#contentsRight > div.wrapperCenter.mt10 > a.buttonLBright.lastchild").Click()
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = page.Find("#navigation > ul > li.lastchild").Click()
+
+	if err != nil {
+		driver.Stop()
+		log.Println(err)
+		return err.Error()
+	}
+
+	err = driver.Stop()
+	if err != nil {
+		log.Println(err)
+		return err.Error()
+	}
+	return result
 }
